41bdafd5fd7694dcba158f09b6ef37cbdb698758
TikeshKardate <tikesh.kardate@gmail.com>
2025-03-06 14:44:21 +0530

    [Tikesh] Add. User Input added to say hello to user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'HelloWorldApp';
   imgUrl= "../assets/BL_logo_square_jpg.jpg";
   url="https://www.bridgelabz.com";
+  userName: string = "";
 
   ngOnInit() : void{
     this.title = "Hello from BridgeLabz.";
@@ -19,5 +23,10 @@ export class AppComponent {
 onClick(){
   console.log("Save button is clicked!");
   window.open(this.url, "_blank");
+}
+
+onInput(): void{
+  console.log("Change event occured");
+  const nameRegex = /^[A-Z][a-zA-Z\s]{2,}$/;
 }
 }
